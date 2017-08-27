b1bfb0940134de8b64b88873f69968c7641784fa
Ajinkya <ajinkyakathe013@gmail.com>
2017-08-27 17:06:23 +0530

    Solved query issue for PG database

--- app/models/project.rb
+++ app/models/project.rb
@@ -5,7 +5,7 @@ class Project < ActiveRecord::Base
 
 	def self.show_project_report(project_ids)
 		UserTask.joins("INNER JOIN tasks as t on t.id = user_tasks.task_id INNER JOIN projects as pr on pr.id = t.project_id")
-		.select('pr.name, sum( CASE WHEN user_tasks.status = "NOT_STARTED" THEN 1 ELSE 0 end) as not_started, sum(CASE WHEN user_tasks.status = "IN_PROGRESS" THEN 1 ELSE 0 END) as in_progress, sum(case when user_tasks.status = "DONE" THEN 1 ELSE 0 END) as done').where("t.project_id IN (?)",project_ids).group("pr.name")
+		.select("pr.name, sum( CASE WHEN user_tasks.status='NOT_STARTED' THEN 1 ELSE 0 end) as not_started, sum(CASE WHEN user_tasks.status = 'IN_PROGRESS' THEN 1 ELSE 0 END) as in_progress, sum(case when user_tasks.status = 'DONE' THEN 1 ELSE 0 END) as done").where("t.project_id IN (?)",project_ids).group("pr.name")
 	end
 
 end
